14f84b1327b2003d3772deab105191159a5f219d
Abubakar-Ciise <abubakarciise4@gmail.com>
2024-07-27 20:44:16 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        // List external dependencies that should not be bundled
+        '@reduxjs/toolkit',
+      ],
+      output: {
+        // Ensure that dependencies are properly resolved
+        globals: {
+          '@reduxjs/toolkit': 'ReduxToolkit',
+        },
+      },
+    },
+  },
+});
