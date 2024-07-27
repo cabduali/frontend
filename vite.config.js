93d25bcb655cd167507cdc964274eb9725afff09
Abubakar-Ciise <abubakarciise4@gmail.com>
2024-07-27 21:20:58 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-router-dom',
-        '@mui/material',
-        '@emotion/react',
-        '@emotion/styled',
-        '@mui/icons-material',
-      ],
-    },
-  },
 });
